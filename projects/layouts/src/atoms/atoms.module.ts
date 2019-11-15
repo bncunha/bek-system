b68ebe1bbc734d2559519519f31f1972527277bd
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2019-11-15 03:06:12 -0300

    colocado botão de ação

--- projects/layouts/src/atoms/atoms.module.ts
+++ projects/layouts/src/atoms/atoms.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TituloUmComponent } from './titulo-um/titulo-um.component';
 import { CardComponent } from './card/card.component';
 import { TituloDoisComponent } from './titulo-dois/titulo-dois.component';
+import { BtnPrimarioComponent } from './btn-primario/btn-primario.component';
 
 @NgModule({
-  declarations: [TituloUmComponent, CardComponent, TituloDoisComponent],
+  declarations: [TituloUmComponent, CardComponent, TituloDoisComponent, BtnPrimarioComponent],
   imports: [
     CommonModule
   ],
-  exports: [TituloUmComponent, CardComponent, TituloDoisComponent]
+  exports: [TituloUmComponent, CardComponent, TituloDoisComponent, BtnPrimarioComponent]
 })
 export class AtomsModule { }
---
+++ projects/layouts/src/atoms/btn-primario/btn-primario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-btn-primario',
+  templateUrl: './btn-primario.component.html',
+  styleUrls: ['./btn-primario.component.scss'],
+})
+export class BtnPrimarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
